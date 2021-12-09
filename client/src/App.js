5a5fe5b84bdff37f6c98bf971c88f40e17091bf1
Austin <austinchoi22@gmail.com>
2021-12-09 14:13:28 -0700

    adjustment on index and app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router";
+import AppointmentsAndMore from "./pages/AppointmentsAndMore";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Container>
+        <Switch>
+          {/* <Route exact path="/" component={} /> */}
+          <Route exact path="/appointments" component={AppointmentsAndMore} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- client/src/pages/AppointmentsAndMore.js
+++ client/src/pages/AppointmentsAndMore.js
@@ -45,3 +45,5 @@ const AppointmentsAndMore = () => {
   )
 
 }
+
+export default AppointmentsAndMore;
